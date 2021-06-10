cedbc607738ccac7502fb8ac922bcb40cae49448
Derrick <derrick.chow234@gmail.com>
2021-06-09 23:53:15 -0400

    Changed css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>News App</h1>
       <div>
         <h2>The Guardian</h2>
+        <h2>New York Times</h2>
+        <h2>Reuters</h2>
       </div>
     </div>
   );
