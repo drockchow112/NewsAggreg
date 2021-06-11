d266ace1d931c25f29cefb83d6f10d4d3344517e
Derrick <derrick.chow234@gmail.com>
2021-06-10 23:57:23 -0400

    changed layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>News App</h1>
-      <div>
+      <div className="newsbar">
         <h2>The Guardian</h2>
         <h2>New York Times</h2>
         <h2>Reuters</h2>
+        <h3></h3>
       </div>
     </div>
   );
